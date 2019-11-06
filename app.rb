abdcea3935083f15a0329790f054604743b782a2
jose luis <cortezluisjose@gmail.com>
2019-11-06 07:40:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  class CreateDogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+ 
+end
 end
